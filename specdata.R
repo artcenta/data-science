5ec6626c019e7def5ff7dbe77fb1c36f4ee4869a
Justin GOUEDE DOHO <jdgouede@artcenta.com>
2017-01-30 21:50:23 +0100

    Add files via upload

--- specdata.R
+++ specdata.R
@@ -9,5 +9,5 @@ specdata.extract<- function(i, my.directory){
   file.list = list.files(my.directory,pattern = my.pattern)
   my.file = file.list[1]
   my.path = concat(my.directory,"/", my.file)
-  do.call(rbind, lapply(my.file, function(x) read.csv(my.path, header=TRUE)))
+  read.csv(my.path, header=TRUE)
 }
